75e7479e1fa66d9d6ec0a4c7b0f44c9f0b1b54ff
DipuBrahmbhatt <drdnb24@gmail.com>
2025-03-30 19:05:59 +0530

    Add files via upload
    Awesome Quiz App Using HTML,CSS and JS

---
+++ Web-Projects/Quiz-App/JS/questions.js
@@ -0,0 +1,60 @@
+// creating an array passing the number , questions, options and answers
+
+let questions = [
+    {
+        numb:1,
+        question: "What does HTMl stand for?",
+        answer:"Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question: "What does CSS stand for?",
+        answer:"Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Comuter Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb:3,
+        question: "What does PHP stand for?",
+        answer:"Hypertext Preprocessor",
+        options: [
+            "Hypertext programming",
+            "Hypertext Preprocessor",
+            "Hypertext Preprogramming",
+            "Hypertext processer"
+        ]
+    },
+    {
+        numb:4,
+        question: "What does SQl stand for?",
+        answer:"Structured Query Language",
+        options: [
+            "Stylish Query Language",
+            "Executable Multiple Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb:5,
+        question: "What does XML stand for?",
+        answer:"eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXtra Multi Program Language",
+            "eXamine Multiple language"
+        ]
+    },
+
+];
